36a5aead6aabb7ea8b9b1b2a2323f3f69fe81961
Jay Palekar <me@jaypalekar.com>
2024-04-13 21:31:55 -0400

    Add ubuntu machine config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of jaypalekar";
+  description = "Home Manager";
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -24,6 +24,20 @@
         extraSpecialArgs = {
           username = "jaypalekar";
           homeDirectory = "/Users/jaypalekar";
+        };
+      };
+      homeConfigurations."jay" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = {
+          username = "jay";
+          homeDirectory = "/home/jay";
         };
       };
     };
