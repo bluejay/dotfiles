43828058b048db473d9ac2afcaeeef0e1017c0c2
Jay Palekar <me@jaypalekar.com>
2024-04-14 21:21:58 -0400

    Add module for linux only config

--- flake.nix
+++ flake.nix
@@ -31,7 +31,10 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./home.nix ];
+        modules = [ 
+          ./home.nix 
+          ./linux.nix
+        ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
